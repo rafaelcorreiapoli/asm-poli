dd54b63153e3bdf26d867258ccfc31b19e16f980
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-06-29 14:49:16 -0300

    FINAL COM COMMENTS

--- p1.s
+++ p1.s
@@ -5,28 +5,25 @@ numero: .word 20
 size: .word 1
 
 main:
-            LDR   r1, =0x1E @ 30 em hexa
-            LDR   r8, =lista @ ponteiro para a fila de primos
-            LDR   r9, size
-            LDR r7, numero
-            BL fatora
-            B done
+            LDR   r8, =lista @ ponteiro para lista final
+            LDR   r9, size  @ valor do size
+            LDR r7, numero @ numero
+            BL fatora @ vai pro fatora
+            B done  @ depois que fatorar vai pro done
 
 fatora:
-  MOV r4, #2
-
-
+  MOV r4, #2    @inicia i
 loop:
-  CMP r4, r7
-  BGT done
-  MOV r1, r7 @  20
-  MOV r2, r4  @  i
-  BL divide
-  CMP r1, #0
-  MOVEQ r7, r0
-  STREQ r4, [r8], #4
-  ADDEQ r9, r9, #1
-  ADDNE r4, r4, #1
+  CMP r4, r7  @ compara i com numero
+  BGT done    @ se for maior, acabou o while
+  MOV r1, r7 @  coloca numero no divisor
+  MOV r2, r4  @  coloca i no dividendo
+  BL divide @ chama rotina de dividir
+  CMP r1, #0  @r1 tem o resto, comparo com 0
+  MOVEQ r7, r0  @ se for 0, copia o quociente inteiro da divisao pro r7 (que se torna o novo numero)
+  STREQ r4, [r8], #4  @ guardo o i na lista e incremento o ponteiro pra lista em #4 (1  word)
+  ADDEQ r9, r9, #1  @ somo um no contador de números da lista (size)
+  ADDNE r4, r4, #1  @ se o resto da divisão não for 0, incremento 1 no i
 
   B loop
 
@@ -58,5 +55,5 @@ divide_next:
 
 done:
           SUB r9, r9, #1
-          @ EM r9 tem o size, mas precisa guardar ele no endereço size?
+          STR r9, size  @ guardo total de numeros em size
             SWI         0x123456 @ End
